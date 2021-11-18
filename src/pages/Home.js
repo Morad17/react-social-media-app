31d43469b150b4ae5260e86c5b215c02dd97da3a
Morad17 <morad123@hotmail.co.uk>
2021-11-18 22:23:23 +0000

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            
+            <h1>Register</h1>
+            <form action="/">
+                <label htmlFor="">Username</label>
+                <input type="text" />
+                <label htmlFor="">Password</label>
+                <input type="text" />
+                <label htmlFor="">Email</label>
+                <input type="text" />
+            </form>
         </div>
     )
 }
